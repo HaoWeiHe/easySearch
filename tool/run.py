fab5cac94f94b3180270005ee4031c6277a23460
HaoWeiHe <haoweihohoho@gmail.com>
2021-09-18 12:20:39 -0700

    fix OOV & show the higest files only once throughout the result

--- tool/run.py
+++ tool/run.py
@@ -2,37 +2,55 @@ from gensim.utils import tokenize
 from  heapq import heappop, heappush
 
 class earsySearch():
-	def __init__(self):
-		self._num = 10
+	def __init__(self, NUM = 10):
+		self._num = NUM 
 		self.glove_input_file = 'model/glove.6B.200d.txt'
 		self.word2vec_output_file = 'model/glove.6B.200d.txt.w2v'
 		self.model = self.load_mode()
-	
+		self.ignore = ["data/.keep"]
+
 	def train(self):
 		from gensim.scripts.glove2word2vec import glove2word2vec
 		glove2word2vec(self.glove_input_file , self.word2vec_output_file)
 
 	def load_mode(self):
 		from gensim.models import KeyedVectors
-		model = KeyedVectors.load_word2vec_format(self.word2vec_output_file, binary=False,limit=500000)
+		model = KeyedVectors.load_word2vec_format(self.word2vec_output_file, binary = False, limit = 500000)
 		return model
 
 	def search(self, target_word = ""):
 		import os
-		h = []
+		
+		if target_word not in self.model.wv:
+			target_word = target_word.lower()
+		if target_word not in self.model.wv:
+			return "OOV"
+
+		h = [] 
 		for root, _, files in os.walk("data", topdown=False):
 			for name in files:
+				hi_score, rtr_w = 0, ""
 				filename = os.path.join(root, name)
 				with open(filename) as file:  
+					if filename in self.ignore :
+						continue
 					data = file.read() 
 					for w in set(list(tokenize(data, deacc = True))):
+						#OOV 
+						if w not in self.model.wv:
+							continue
 						score = self.model.wv.similarity(target_word, w)
-						heappush(h,(score, w, filename))
-						if len(h) > self._num:
-							heappop(h)
+						if score > hi_score:
+							hi_score = score
+							rtr_w = w
+				heappush(h,(hi_score,filename))
+				if len(h) > self._num:
+					heappop(h)
 		return h
 
 	def beautifulPrint(self, h):
 		while h:
 			data = heappop(h)
-			print("{}:\t{}\tscore:{}".format(data[2],data[1],data[0]))
+			if data == "OOV":
+				print("sorry, the input query doesn't exist in our dictionary")
+			print("{}:\tscore:{:0.3f}".format(data[1],data[0]))
